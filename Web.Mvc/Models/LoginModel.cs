eac4b8435de3c2318db0279ed2094316dbf67ccf
SamuelRBatista <samuka-srb@live.com>
2025-03-19 18:04:40 -0300

    refactor:19032025 => Model de login

--- Web.Mvc/Models/LoginModel.cs
+++ Web.Mvc/Models/LoginModel.cs
@@ -1,8 +1,15 @@
 namespace Web.Mvc.Models
 {
     public class LoginModel
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+
+    public LoginModel(string? username, string? password)
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        Username = username;
+        Password = password;
     }
+}
+
 }
