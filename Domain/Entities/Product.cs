de43e314f44e2ce1e1f485bb20958743052c472e
SamuelRBatista <samuel.r.batista1983@gmail.com>
2025-03-31 11:48:16 -0300

    chore:31032025 => Corrreção do atributos preço

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace Domain.Entities;
@@ -7,6 +8,7 @@ public class Product
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
     public string Description {get; set;} = string.Empty;
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Price {get; set;}
     public string Sku {get; set;} = string.Empty;
     public string BarCode {get; set;} = string.Empty;
